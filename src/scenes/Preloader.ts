a3798bdcccb0aa364da12c27548c2ce62bcceee6
PatBG <pat@patbgames.com>
2024-04-14 15:30:19 +0200

    Added nineSlice button

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -34,9 +34,8 @@ export class Preloader extends Scene
         this.load.setPath('assets');
 
         this.load.image('logo', 'logo.png');
-
+        this.load.image('button', 'button.png');
         this.load.spritesheet('characters', 'characters.png', { frameWidth: 80, frameHeight: 80 });
-
     }
 
     create ()
