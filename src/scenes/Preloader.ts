bc437bd83d4ef1f846f683272d23e9ea67484163
PatBG <pat@patbgames.com>
2024-04-14 23:12:22 +0200

    Changed background

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -1,35 +1,32 @@
 import { Scene } from 'phaser';
 import { Global } from '../Global';
 
-export class Preloader extends Scene
-{
-    constructor ()
-    {
+export class Preloader extends Scene {
+    constructor() {
         super('Preloader');
     }
 
-    init ()
-    {
+    init() {
         //  We loaded this image in our Boot Scene, so we can display it here
-        this.add.image(Global.SCREEN_CENTER_X, Global.SCREEN_CENTER_Y, 'background').setScale(1.7);
+        this.add.image(Global.SCREEN_CENTER_X, Global.SCREEN_CENTER_Y, 'background')
+            .setScale(Global.SCREEN_WIDTH / 8, 1.7)
+            .setAlpha(0.5);
 
         //  A simple progress bar. This is the outline of the bar.
         this.add.rectangle(Global.SCREEN_CENTER_X, Global.SCREEN_CENTER_Y, 468, 32).setStrokeStyle(1, 0xffffff);
 
         //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(Global.SCREEN_CENTER_X-230, Global.SCREEN_CENTER_Y, 4, 28, 0xffffff);
+        const bar = this.add.rectangle(Global.SCREEN_CENTER_X - 230, Global.SCREEN_CENTER_Y, 4, 28, 0xffffff);
 
         //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
         this.load.on('progress', (progress: number) => {
-
             //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
             bar.width = 4 + (460 * progress);
 
         });
     }
 
-    preload ()
-    {
+    preload() {
         //  Load the assets for the game - Replace with your own assets
         this.load.setPath('assets');
 
@@ -42,10 +39,11 @@ export class Preloader extends Scene
         this.load.image('button', 'button.png');
 
         this.load.spritesheet('characters', 'characters.png', { frameWidth: 80, frameHeight: 80 });
+
+        this.load.audio('button', 'button.wav');
     }
 
-    create ()
-    {
+    create() {
         //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
         //  For example, you can define global animations here, so we can use them in other scenes.
 
