fbc7a4efec6ef78b4d8cf4314375162106038e4e
PatBG <pat@patbgames.com>
2024-04-14 23:12:02 +0200

    Changed background and added sounds

--- src/scenes/Help.ts
+++ src/scenes/Help.ts
@@ -2,8 +2,8 @@ import { Scene } from 'phaser';
 import { Global } from '../Global';
 
 export class Help extends Scene {
-    camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
+    buttonSound: Phaser.Sound.BaseSound;
 
     indexPage = 0;
     helpPage1: Phaser.GameObjects.Image;
@@ -15,11 +15,11 @@ export class Help extends Scene {
     }
 
     create() {
-        this.camera = this.cameras.main
-        this.camera.setBackgroundColor(0xff0000);
+        this.buttonSound = this.sound.add('button');
 
-        this.background = this.add.image(Global.SCREEN_CENTER_X, Global.SCREEN_CENTER_Y, 'background').setScale(1.7);
-        this.background.setAlpha(0.5);
+        this.background = this.add.image(Global.SCREEN_CENTER_X, Global.SCREEN_CENTER_Y, 'background')
+            .setScale(Global.SCREEN_WIDTH / 8, 1.7)
+            .setAlpha(0.5);
 
         const buttonPreviousX = Global.SCREEN_CENTER_X - 200;
         const buttonNextX = Global.SCREEN_CENTER_X + 200;
@@ -38,22 +38,26 @@ export class Help extends Scene {
 
         this.add.nineslice(buttonPreviousX, buttonY, 'button', 0, 200, 100, 30, 30, 30, 30)
             .setInteractive()
-            .on('pointerup', () => this.changePage(-1));
+            .on('pointerup', () => { this.changePage(-1) });
         this.add.text(buttonPreviousX, buttonY, 'Previous', Global.MENU_STYLE).setOrigin(0.5);
 
         this.add.nineslice(buttonNextX, buttonY, 'button', 0, 200, 100, 30, 30, 30, 30)
             .setInteractive()
-            .on('pointerup', () => this.changePage(1));
+            .on('pointerup', () => { this.changePage(1) });
         this.add.text(buttonNextX, buttonY, 'Next', Global.MENU_STYLE).setOrigin(0.5);
 
         this.add.nineslice(buttonBackX, buttonBackY, 'button', 0, 200, 100, 30, 30, 30, 30)
             .setInteractive()
-            .on('pointerup', () => this.scene.start('MainMenu'));
+            .on('pointerup', () => { this.buttonSound.play(); this.indexPage = 0; this.scene.start('MainMenu') });
         this.add.text(buttonBackX, buttonBackY, 'Back', Global.MENU_STYLE).setOrigin(0.5);
     }
 
     changePage(inc: number) {
+        const previousPage = this.indexPage;
         this.indexPage = Math.min(Math.max(this.indexPage + inc, 0), 2);
+        if (inc != 0 && previousPage != this.indexPage) {
+            this.buttonSound.play();
+        }
         this.helpPage1.setVisible(this.indexPage == 0);
         this.helpPage2.setVisible(this.indexPage == 1);
         this.helpPage3.setVisible(this.indexPage == 2);
--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -3,28 +3,39 @@ import { Global } from '../Global';
 
 export class MainMenu extends Scene {
     background: GameObjects.Image;
-    logo: GameObjects.Image;
-    title: GameObjects.Text;
+    buttonSound: Phaser.Sound.BaseSound;
 
     constructor() {
         super('MainMenu');
     }
 
     create() {
-        this.background = this.add.image(Global.SCREEN_CENTER_X, Global.SCREEN_CENTER_Y, 'background').setScale(1.7);
+        this.buttonSound = this.sound.add('button');
 
-        this.logo = this.add.image(Global.SCREEN_CENTER_X, Global.SCREEN_CENTER_Y, 'logo');
+        this.background = this.add.image(Global.SCREEN_CENTER_X, Global.SCREEN_CENTER_Y, 'background')
+            .setScale(Global.SCREEN_WIDTH / 8, 1.7)
+            .setAlpha(0.5);
 
-        this.title = this.add.text(Global.SCREEN_CENTER_X, Global.SCREEN_CENTER_Y + 160, 'Main Menu', {
-            fontFamily: Global.FONT_FAMILY, fontSize: Global.FONT_SIZE, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+        this.add.image(Global.SCREEN_WIDTH - 20, Global.SCREEN_HEIGHT - 20, 'logo').setOrigin(1).setScale(0.5);
 
-        this.input.once('pointerdown', () => {
+        const buttonX = Global.SCREEN_CENTER_X;
+        const button1Y = Global.SCREEN_CENTER_Y + 100;
+        const button2Y = button1Y + 150;
 
-            this.scene.start('Game');
+        this.add.text(Global.SCREEN_CENTER_X, 20,
+            "RUNES SUMMONER\n\n"
+            + "LudumDare#55\nApril 14, 2024\nTheme: Summoning\n\n"
+            + "Compo entry\nby PatBG", Global.MENU_STYLE)
+            .setOrigin(0.5, 0);
 
-        });
+        this.add.nineslice(buttonX, button1Y, 'button', 0, 400, 100, 30, 30, 30, 30)
+            .setInteractive()
+            .on('pointerup', () => { this.buttonSound.play(); this.scene.start('Game') });
+        this.add.text(buttonX, button1Y, 'Play', Global.MENU_STYLE).setOrigin(0.5);
+
+        this.add.nineslice(buttonX, button2Y, 'button', 0, 400, 100, 30, 30, 30, 30)
+            .setInteractive()
+            .on('pointerup', () => { this.buttonSound.play(); this.scene.start('Help') });
+        this.add.text(buttonX, button2Y, 'Help', Global.MENU_STYLE).setOrigin(0.5);
     }
 }
